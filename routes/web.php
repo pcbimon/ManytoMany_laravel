a0dd23a3045c25a618476eb5d71c01a3614bbde0
pcbimon <pcbimon@gmail.com>
2017-01-07 15:13:08 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Role;
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +14,37 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/create', function()
+{
+  $user = User::find(1);
+  $role = new Role(['name'=>'Administator']);
+  $user->roles()->save($role);
+});
+Route::get('/read', function()
+{
+  $user = User::findOrfail(1);
+  foreach ($user->roles as $role) {
+    return $role;//or echo
+  }
+});
+Route::get('/update', function()
+{
+  $user = User::findOrfail(1);
+  if($user->has('roles')){
+    foreach ($user->roles as $role) {
+      if ($role->name == 'Administator') {
+        $role->name = 'Subscriber';
+        $role->save();
+      }
+    }
+  }
+});
+Route::get('/delete', function()
+{
+  $user = User::findOrfail(1);
+  // $user->roles()->delete();
+  foreach ($user->roles as $role) {
+    $role->where('id', 2)->delete();
+  }
 });
